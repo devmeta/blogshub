b20d382bf0b0e3f9eaf5862a159e7dbcbac40f59
MartinFrith <telemagico@gmail.com>
2015-10-08 15:01:04 -0300

    config base url

--- public/min/groupsConfig.php
+++ public/min/groupsConfig.php
@@ -34,22 +34,7 @@ return array(
         '//assets/css/ionicons.min.css', 
         '//assets/css/slick.css',
         '//assets/css/theme.css',
-        '//assets/css/foam.css'
-    ),
-    'js.admin' => array(
-        '//assets/js/jquery-2.1.4.min.js',
-        '//assets/js/jquery-ui-1.10.4.custom.min.js',
-        '//assets/js/bootstrap.min.js',
-        '//assets/js/summernote.min.js',
-        '//assets/js/dropzone.js',
-        '//assets/js/admin/app.js'
-    ),
-    'css.admin' => array(
-        '//assets/css/bootstrap.min.css', 
-        '//assets/css/font-awesome.min.css', 
-        '//assets/css/ionicons.min.css', 
-        '//assets/css/summernote.css', 
-        '//assets/css/dropzone.css', 
-        '//assets/css/theme.css'
+        '//assets/css/foam.css',
+        '//assets/css/circle-tiles.css'        
     )
 );
