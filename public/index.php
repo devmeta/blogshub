6d3b9d17165893ba1c72b5d021158a29cf35ee8d
MartinFrith <telemagico@gmail.com>
2015-10-17 07:28:37 -0300

    Cache config

--- public/index.php
+++ public/index.php
@@ -4,19 +4,24 @@ require('../bootstrap/bootstrap.php');
 require('../config/common.php');
 require('../config/routes.php');
 
-$app = new \Bootie\App;
+/*
+ * If cache option is enabled AND this url is grabbed
+ * then execution stops on the next line
+ */
+
+if( config()->cache_enabled ) \Bootie\Cache::init(config()->cache,PATH);
+
 $db = null;
+$app = new \Bootie\App;
 
 try 
 {
-	$app->handle(PATH);
+	$app->run(PATH);
 } 
 catch (Exception $e) 
 {
 	\Bootie\Error::exception($e);
 }
 
-if($debug){
-	include __DIR__.'/../bootstrap/debug.php';
-}
-
+if( config()->cache_enabled ) \Bootie\Cache::store();
+if( config()->debug ) include __DIR__.'/../bootstrap/debug.php';
