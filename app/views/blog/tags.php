21823e8987ec01a2a3cc27ed9aed155e8f56a15e
MartinFrith <telemagico@gmail.com>
2015-10-17 21:13:03 -0300

    foam tiles sputnik titles style

--- app/views/blog/tags.php
+++ app/views/blog/tags.php
@@ -10,8 +10,8 @@
                 <li>
                     <a href="/<?php echo $post->slug;?>" class="feature-olivia col-md-4 col-sm-6 col-xs-6" style="background-image: url(<?php echo config()->baseurl;?>/upload/posts/th-<?php echo count($post->files()) ? $post->files()[0]->name : 'default';?>)">
                         <div class="foam<?php echo $i%6+1;?> overlay"></div>
-                        <span class="feature-title"><?php echo $post->title;?></span>
-                        <span class="feature-descrip"><?php echo words($post->caption,15);?></span>
+                        <span class="feature-title"><span><?php echo implode('</span><span>',explode(' ',words($post->title,10)));?></span></span>
+                        <span class="feature-descrip"><span><?php echo implode('</span><span>',explode(' ',words($post->caption,18)));?></span></span>
                         <span class="feature-attr">
                             <i class="ion-android-time"></i> <?php echo timespan($post->created);?>
                             <i class="ion-eye"></i> <?php echo $post->hits;?>
