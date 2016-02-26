a2fdec7c9b88e558ae5f095442c2b2a61ee19d05
MartinFrith <telemagico@gmail.com>
2016-02-25 22:06:53 -0300

    entry typos

--- app/views/blog/entry.php
+++ app/views/blog/entry.php
@@ -1,9 +1,9 @@
 <div class="col-md-9">
   	<h1 class="page-header"><?php echo $entry->title;?></h1>
-    <blockquote><em><?php echo $entry->caption;?><br><?php echo date('d M Y',$entry->created);?></em></blockquote>
+    <blockquote><em><?php echo $entry->caption;?><br><?php echo date('d M Y',$entry->created);?></em> <?php echo urlencode(PATH);?></blockquote>
 
 <?php if($entry->tags()):?>
-    <div class="alert">
+    <div class="alert"> 
     <?php foreach($entry->tags() as $tag) : if( ! isset($tag->tag)) continue;?>
         <a href="/tag/<?php echo $tag->tag;?>" class="label label-success label-badge btn-tag-included"><?php echo $tag->tag;?></a>
     <?php endforeach;?>
@@ -11,7 +11,7 @@
 <?php endif;?>
 
     <div class="social">
-        <a target="_blank" class="facebook" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode(PATH);?>" title="Facebook" data-external="true" data-placement="top"><i class="ion-social-facebook"></i><span class="badge social-count fbcnt"></span></a>
+        <a target="_blank" class="facebook" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode(PATH);?>" title="Facebook" data-external="true" data-placement="top"><i class="ion-social-facebook"></i><span class="badge social-count fbcnt"></span> </a>
         <a target="_blank" class="twitter" href="https://twitter.com/intent/tweet?text=<?php print $entry->title;?> <?php echo PATH;?>" title="Twitter" data-external="true" data-placement="top"><i class="ion-social-twitter"></i><span class="badge social-count twcnt"></span></a>
         <a target="_blank" class="googleplus" href="https://plus.google.com/share?url=<?php echo PATH;?>" title="Google+" data-external="true" data-placement="top"><i class="ion-social-googleplus"></i></a>
         <a target="_blank" class="linkedin" href="https://www.linkedin.com/cws/share?url=<?php echo PATH;?>" title="Linkedin" data-external="true" data-placement="top"><i class="ion-social-linkedin"></i></a>
