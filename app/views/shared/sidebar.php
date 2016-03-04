5e9ca1dd72a74830d3038417a52a41e56987fdbc
MartinFrith <telemagico@gmail.com>
2016-03-03 21:21:39 -0300

    sidebar ad

--- app/views/shared/sidebar.php
+++ app/views/shared/sidebar.php
@@ -17,5 +17,5 @@
     
     <div class="clearfix"></div>
     <div class="group-control">&nbsp;</div>
-    <div style="background:url(http://blogs.devmeta.net/upload/posts/th-fist-of-power-e1366397947671_rs.jpg) no-repeat;width:225px;height:225px;padding:25px;text-align:center"><ul style="color:#fff;text-shadow:0 0 2px #000;background-color:rgba(51,51,51,0.5);text-align:left;padding:30px;margin-bottom:30px;"><li>Gratis</li><li>Sin publicidad</li></ul><a href="" class="btn btn-success">Regístra tu Blog!</a></div>
+    <div style="background:url(http://blogs.devmeta.net/upload/posts/th-fist-of-power-e1366397947671_rs.jpg) no-repeat;width:225px;height:225px;padding:25px;text-align:center"><ul style="color:#fff;text-shadow:0 0 2px #000;background-color:rgba(51,51,51,0.5);text-align:left;padding:30px;margin-bottom:30px;"><li>Gratis</li><li>Sin publicidad</li></ul><a href="http://blogs.devmeta.net/" class="btn btn-success" target="_blank">Regístra tu Blog!</a></div>
        
