11f20137682d5834e02a7b458e5cd91c58370419
martin <telemagico@gmail.com>
2016-06-02 20:35:32 -0300

    remove ad space from entry

--- app/views/shared/sidebar.php
+++ app/views/shared/sidebar.php
@@ -15,7 +15,5 @@
     </div>
 <?php endif;?>
     
-    <div class="clearfix"></div>
-    <div class="group-control">&nbsp;</div>
-    <a href="http://blogs.devmeta.net/" target="_blank"><div style="background:url(http://blogs.devmeta.net/upload/posts/th-fist-of-power-e1366397947671_rs.jpg) no-repeat;width:225px;height:225px;padding:15px;text-align:center;-webkit-filter: saturate(7);filter: saturate(7);"><ul style="list-style:none;color:#333;text-shadow:0 0 1px #fff;font-weight:bold;box-shadow:0 0 3px rgba(0,0,0,0.5);background-color:rgba(250,250,250,0.9);text-align:left;padding:30px;margin-bottom:0;"><li>100% Gratuito</li><li>+ Comentarios</li><li>+ Redes</li><li>Sin publicidad</li><li>&nbsp;</li><li><label class="btn btn-danger"> Registra tu Blog!</label></li></ul></div></a>
+
        
