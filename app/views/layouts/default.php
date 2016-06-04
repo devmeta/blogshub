ae2318ca9e3fefcfc5973d3d27ba25862ce0fd5e
martin <telemagico@gmail.com>
2016-06-04 16:45:21 -0300

    avatar upload

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -9,11 +9,12 @@
     <meta property="og:title" content="<?php echo isset($meta->og_title) ? $meta->og_title : '';?>" />
     <meta property="og:description" content="<?php echo isset($meta->og_description) ? $meta->og_description : '';?>" />
     <meta property="og:image" itemprop="image primaryImageOfPage" content="<?php echo isset($meta->og_image) ? $meta->og_image : '';?>" />
- 	<link rel="shortcut icon" href="/assets/favicon.ico" rel="icon" type="image/x-icon" />
+ 	<link rel="shortcut icon" href="<?php echo ! empty(config('blog')->data->avatar) ? config()->baseurl . '/upload/profile/ty-' . config('blog')->data->avatar : '/assets/favicon.ico';?>" rel="icon" type="image/x-icon" />
 	<link rel="stylesheet" type="text/css" href="/min/?g=css.default"> 
 	<link rel="stylesheet" type="text/css" href="/assets/css/<?php echo config('blog')->data->style->name;?>.css"> 
 	<script type="text/javascript" src="/min/?g=js.default"></script>
 </head>
+
 <body class="default">
 	<?php include SP . 'app/views/shared/header.php';?>
 	<div class="container">
